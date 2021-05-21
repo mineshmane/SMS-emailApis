fc2c6cb746029b8dc9e262c760e20ea8b7be16b7
mineshmane <mineshmane94@gmail.com>
2021-05-21 17:42:27 +0530

    Added

--- configu/config.js
+++ configu/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoUrl:'mongodb://localhost:27017/UserData'
+    // mongoUrl:'mongodb://localhost:27017/UserData'
+    mongoUrl:'mongodb+srv://mineshmane:<Rahul@12345>@cluster0.kqlmp.mongodb.net/UserdataBase?retryWrites=true&w=majority'
 }
