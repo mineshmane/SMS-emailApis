c0ea1ff4f538a8a210f00463837f03fd3db6ebc1
mineshmane <mineshmane94@gmail.com>
2021-05-21 12:45:22 +0530

    Added

--- swaggerui/swaggerapi.js
+++ swaggerui/swaggerapi.js
@@ -6,7 +6,7 @@ module.exports = swaggerDefinition = {
         "version": "1.0.0",
         "title": "Swagger Email -sms verification API Documentation"
     },
-    "host": 'email-apis.herokuapp.com/',
+    "host": 'email-apis.herokuapp.com',
     "basePath": "/",
     "tags": [
         {
